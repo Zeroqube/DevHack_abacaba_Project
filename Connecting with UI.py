2c4d43a55c805243d39122433c97cb328e39917e
Denis <denacua5@gmail.com>
2020-12-19 14:06:03 +0300

    Test interface

--- Connecting with UI.py
+++ Connecting with UI.py
@@ -1,5 +1,5 @@
 import sys
-
+from PyQt5.QtGui import QPixmap
 from PyQt5 import uic  # Импортируем uic
 from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog
 
@@ -9,11 +9,19 @@ class MyWidget(QMainWindow):
         super().__init__()
         uic.loadUi('Interface.ui', self)  # Загружаем дизайн
         self.outsideImage.clicked.connect(self.convert)
+        self.outsideText.clicked.connect(self.get_text)
         # Обратите внимание: имя элемента такое же как в QTDesigner
 
     def convert(self):
-        fname = QFileDialog.getOpenFileName(self, 'Выбрать картинку', '')[0]
-        print(fname)
+        image_name = QFileDialog.getOpenFileName(self, 'Выбрать картинку', '', 'Картинка (*.jpg);;Все файлы (*)')[0]
+        self.pixmap = QPixmap(image_name)
+        self.userImage.setPixmap(self.pixmap)
+
+    def get_text(self):
+        text_name = QFileDialog.getOpenFileName(self, 'Выбрать текст', '', 'Текст (*.txt);;Все файлы (*)')[0]
+        with open(text_name) as file:
+            text = file.read()
+            self.plainTextEdit.insertPlainText(text)
 
 
 if __name__ == '__main__':
