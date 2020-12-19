6e182f3e5762e39359f571869d8ea7d181ba672f
Denis <denacua5@gmail.com>
2020-12-19 18:18:49 +0300

    Two-staged Interface

--- Connecting with UI.py
+++ Connecting with UI.py
@@ -8,16 +8,24 @@ class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('Interface.ui', self)  # Загружаем дизайн
-        self.outsideImage.clicked.connect(self.convert)
-        self.outsideText.clicked.connect(self.get_text)
+        self.outsideImage.clicked.connect(self.give_image_to_encrypt)
+        self.outsideText.clicked.connect(self.give_text)
         self.saveImage.clicked.connect(self.get_image)
+        self.upload.clicked.connect(self.give_image_to_decrypt)
 
-    def convert(self):
-        self.image_name = QFileDialog.getOpenFileName(self, 'Выбрать картинку', '', 'Картинка (*.jpg);;Все файлы (*)')[0]
+    def give_image_to_encrypt(self):
+        self.image_name = QFileDialog.getOpenFileName(self, 'Выбрать картинку', '', 'Картинка (*.bmp);;Все файлы (*)')[
+            0]
         self.pixmap = QPixmap(self.image_name)
         self.userImage.setPixmap(self.pixmap)
 
-    def get_text(self):
+    def give_image_to_decrypt(self):
+        self.decrypt_name = \
+            QFileDialog.getOpenFileName(self, 'Выбрать картинку', '', 'Картинка (*.bmp);;Все файлы (*)')[0]
+        self.pixmap1 = QPixmap(self.decrypt_name)
+        self.decryptImage.setPixmap(self.pixmap1)
+
+    def give_text(self):
         text_name = QFileDialog.getOpenFileName(self, 'Выбрать текст', '', 'Текст (*.txt);;Все файлы (*)')[0]
         with open(text_name) as file:
             text = file.read()
