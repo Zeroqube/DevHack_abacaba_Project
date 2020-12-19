073022ec5b1723169219300e8d4603e758d8f804
Denis <denacua5@gmail.com>
2020-12-19 16:55:57 +0300

    Test interface

--- Connecting with UI.py
+++ Connecting with UI.py
@@ -13,8 +13,9 @@ class MyWidget(QMainWindow):
         self.saveImage.clicked.connect(self.get_image)
 
     def convert(self):
-        image_name = QFileDialog.getOpenFileName(self, 'Выбрать картинку', '', 'Картинка (*.jpg);;Все файлы (*)')[0]
-        self.pixmap = QPixmap(image_name)
+        self.image_name = QFileDialog.getOpenFileName(self, 'Выбрать картинку', '', 'Картинка (*.jpg);;Все файлы (*)')[
+            0]
+        self.pixmap = QPixmap(self.image_name)
         self.userImage.setPixmap(self.pixmap)
 
     def get_text(self):
