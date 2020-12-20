906bd87cc73ad2b69edb6948559a1f9c7d6a9a5e
Denis <denacua5@gmail.com>
2020-12-20 15:16:34 +0300

    Ready to merge

--- Connecting with UI.py
+++ Connecting with UI.py
@@ -1,9 +1,10 @@
 import sys
-from PyQt5.QtGui import QPixmap, QActionEvent
+from PyQt5.QtGui import QPixmap
 from PyQt5 import uic  # Импортируем uic
-from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog
+from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QMessageBox
 from random import randint
 from PIL import Image
+from math import factorial
 
 
 class MyWidget(QMainWindow):
@@ -16,19 +17,21 @@ class MyWidget(QMainWindow):
         self.upload.clicked.connect(self.give_image_to_decrypt)
         self.encryptedText.setReadOnly(True)
         self.generator.clicked.connect(self.generate_key)
-        self.encryptedText.setText('Hello World!!!')
-        # self.downloadImage.clicked.connect(self.file_save)
+        # self.encryptedText.setText('Hello World!!!')
+        self.downloadImage.clicked.connect(self.text_save)
         self.saveImage.clicked.connect(self.image_save)
 
     def give_image_to_encrypt(self):
         self.image_name = QFileDialog.getOpenFileName(self, 'Выбрать картинку', '', 'Картинка (*.bmp)')[
             0]
         self.pixmap = QPixmap(self.image_name)
+
         self.userImage.setPixmap(self.pixmap)
 
     def give_image_to_decrypt(self):
         self.decrypt_name = \
-            QFileDialog.getOpenFileName(self, 'Choose picture', '', 'Картинка (*.bmp)')[0]
+            QFileDialog.getOpenFileName(self, 'Choose picture', '',
+                                        'Картинка (*.bmp)')[0]
         self.pixmap1 = QPixmap(self.decrypt_name)
         self.decryptImage.setPixmap(self.pixmap1)
 
@@ -47,13 +50,17 @@ class MyWidget(QMainWindow):
 
     def image_save(self):
         path = \
-            QFileDialog.getSaveFileName(self, 'Save file', '', 'Картинка (*.jpg);;Картинка (*.png);;Картинка (*.bmp)')[
-                0]
-        im = Image.open(path)
-        im.save(im)
+            QFileDialog.getSaveFileName(self, 'Save file', '', 'Картинка (*.bmp)')[0]
+        # im.save(path)
 
     def generate_key(self):
-        self.keyout.setText(str(randint(1000000000, 10000000000)))
+        try:
+            x = self.pixmap.width()
+            y = self.pixmap.height()
+            chars = 100  # количество символов в тексте
+            self.keyout.setText(str(randint(0, factorial(x * y) // (factorial(x * y - chars)))))
+        except AttributeError:
+            QMessageBox.critical(self, "Error!", "Сначала выберите картинку и введите текст для шифровки")
 
 
 if __name__ == '__main__':
