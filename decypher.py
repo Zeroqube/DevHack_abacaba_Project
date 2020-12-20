d242e1b0699472ad7dfb52d06364a46295e73825
Ivan-Pim <snaivini@yandex.ru>
2020-12-20 10:23:29 +0300

    text-to-picture and picture-to-numbers

--- decypher.py
+++ decypher.py
@@ -2,16 +2,42 @@ from PIL import Image
 import random
 
 
+def readpix(x, y):
+    R, G, B = pixels[x, y]
+    ch = ((R & 0x07) << 5) + ((G & 0x03) << 3) + (B & 0x07)
+    return ch
+
+
 def revert(numstr):
     string = ""
     for i in numstr:
         string += i.decode('windows-1251')
     return string
 
-def picture_to_string(key, x, y, n):
+
+def picture_to_string(key, y):
+    global pixels
     numstr = []
     random.seed(key)
-    for i in range(n):
-        p = random.randint(0, x * y)
-        numstr.append(R % 8 + G % 4 + B % 8)
+    visited = set()
+    p = 300
+    while True:
+        #p = random.randint(0, x * y)
+        p += 400
+        if p in visited:
+            continue
+        visited.add(p)
+        ch = readpix(p % y, p // y)
+        if ch == 0x03:
+            break
+        numstr.append(ch)
     return numstr
+
+
+name = input('введите путь к изображению')
+im = Image.open(name)
+pixels = im.load()
+x, y = im.size
+key = input('введите ключ')
+text = picture_to_string(key, y)
+print(str(text))
--- main.py
+++ main.py
@@ -5,9 +5,9 @@ import random
 def change(ch, x, y):
     global pixels
     R, G, B = pixels[x, y]
-    R = R // 8 + ch // 32
-    G = G // 7 + ch % 32 // 8
-    B = B // 8 + ch % 8
+    R = (R & 0xf8) + (ch // 32)
+    G = (G & 0xfc) + ((ch % 32) // 8)
+    B = (B & 0xf8) + (ch % 8)
     pixels[x, y] = (R, G, B)
 
 
@@ -19,51 +19,34 @@ def convert(string):
     return numstr
 
 
-def string_to_picture(key, text, x, y):
+def string_to_picture(key, text, y):
     global pixels
+    visited = set()
     random.seed(key)
-    for c in text:
-        p = random.randint(0, x * y)
-        change(p // y, p % y, c)
-
-
-def change(ch, x, y):
-    global pixels
-    R, G, B = pixels[x, y]
-    R = R // 8 + ch // 32
-    G = G // 7 + ch % 32 // 8
-    B = B // 8 + ch % 8
-    pixels[x, y] = (R, G, B)
-
-
-def convert(string):
-    numstr = []
-    string = string.encode('windows-1251')
-    for i in string:
-        numstr.append(i)
-    return numstr
-
-
-def string_to_picture(key, text, x, y):
-    global pixels
-    random.seed(key)
-    for c in text:
-        p = random.randint(0, x * y)
-        change(p // y, p % y, c)
+    text.append(0x03)  # Признак конца текста
+    i = 0
+    p = 300
+    while True:
+        # p = random.randint(0, x * y)
+        p += 400
+        if p in visited:
+            continue
+        visited.add(p)
+        change(text[i], p % y, p // y)
+        i += 1
+        if text[i - 1] == 0x03:
+            break
 
 
 im2 = Image.new('RGB', (500, 500), (0, 0, 0))
-im2.save('f.JPG')
+im2.save('f.bmp')
 name = input('введите путь к изображению')
 im = Image.open(name)
 pixels = im.load()
 x, y = im.size
-text = input('введите текст')
+text = input('введите текст') + ''
 n_text = convert(text)
 key = input('введите ключ')
-print(pixels[312, 97])
-string_to_picture(key, n_text, x, y)
+string_to_picture(key, n_text, y)
 name = input('введите путь к изображению')
 im.save(name)
-
-
